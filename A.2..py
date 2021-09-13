ee9e2be2ec9d9e2bd3bbbba0e5209cdfa924e50e
aryhalimawan <90484299+aryhalimawan@users.noreply.github.com>
2021-09-13 12:07:56 +0700

    Add files via upload

---
+++ A.2..py
@@ -0,0 +1,61 @@
+import torch as t
+import torchvision.datasets as datasets 
+import torchvision.transforms as transforms
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),])
+
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = t.utils.data.DataLoader(mnist_trainset, batch_size=50, shuffle=True) #50 batch_size
+
+mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+test_loader = t.utils.data.DataLoader(mnist_trainset, batch_size=50, shuffle=True) #50 batch_size, test menggunakan MNIST 60000(Data Train)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.linear1 = nn.Linear(28*28, 100) 
+        self.linear2 = nn.Linear(100, 50) 
+        self.final = nn.Linear(50, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, img): #convert + flatten
+        x = img.view(-1, 28*28)
+        x = self.relu(self.linear1(x))
+        x = self.relu(self.linear2(x))
+        x = self.final(x)
+        return x
+net = Net()
+
+cross_el = nn.CrossEntropyLoss()
+optimizer = t.optim.Adam(net.parameters(), lr=0.001) #Menggunakan optimizer adam, dan learning rate 0.001
+epoch = 20 #Menggunakan 20 epoch
+
+for epoch in range(epoch):
+    net.train()
+
+    for data in train_loader:
+        x, y = data
+        optimizer.zero_grad()
+        output = net(x.view(-1, 28*28))
+        loss = cross_el(output, y)
+        loss.backward()
+        optimizer.step()
+        
+correct=0
+total=0
+with t.no_grad():
+    for data in test_loader:
+        x, y = data
+        output = net(x.view(-1, 784))
+        for idx, i in enumerate(output):
+            if t.argmax(i) == y[idx]:
+                correct +=1
+            total +=1
+print(f'accuracy: {round(correct/total, 3)}')
+
+plt.imshow(x[3].view(28, 28))
+print(t.argmax(net(x[3].view(-1, 784))[0]))
+plt.show()
+
